e4079bf702860c967c8ac54a2a02b9d816da9f27
Leo C_G <84103303+LcgDL@users.noreply.github.com>
2021-11-01 14:24:05 +0100

    Add files via upload

---
+++ socket_portS.py
@@ -0,0 +1,37 @@
+import threading, queue
+from socket import *
+
+t = input(str('Enter host Addrese for scanning:'))
+ib = input('Enter Intervall port beginn: ')
+ie = input('Enter Intervall port End: ')
+t_IP = gethostbyname(t)
+print(f'Starting scan on host: from IP {t}')
+print(f'Starting on port {ib} and ending on {ie}')
+
+# ib = 1
+# ie = 100
+# t_IP = '141.13.99.211' #'pythonprogramming.net'
+
+q = queue.Queue()
+for i in range(int(ib),int(ie)):
+    q.put(i)
+
+def scanning():
+    while not q.empty():
+        i = q.get()
+        s = socket(AF_INET, SOCK_STREAM)
+        try: 
+            c = s.connect_ex((t_IP, i))
+            if c == 0:
+                print(f'Open-Port: {i}')
+        except:
+            print('ERROR')#pass
+        q.task_done()
+
+def thread():
+    for i in range(50):
+        threading.Thread(target=scanning, daemon=True).start()
+
+thread()
+
+q.join()
